4d2919870dce504d026e759e52aab712bca82c36
Pushkraj <pushkraj.lanjekar@weboniselab.com>
2014-06-02 19:01:22 +0530

    First Commit

---
+++ LocalizationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalizationDemo
+//
+//  Created by mac on 02/06/14.
+//  Copyright (c) 2014 Webonise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocalizationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationDemo
+//
+//  Created by mac on 02/06/14.
+//  Copyright (c) 2014 Webonise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *lblTest;
+
+@end
